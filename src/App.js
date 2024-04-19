6e84153d3ee7fd22a2e91916e59d7e4f479fe270
Shashank <113260615+shashankaz@users.noreply.github.com>
2024-04-19 20:34:17 +0530

    back to top

--- src/App.js
+++ src/App.js
@@ -7,10 +7,15 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { FaArrowUp } from "react-icons/fa";
 
 const App = () => {
+  const top = () => {
+    window.scrollTo(0, 0);
+  };
+
   return (
-    <div className="bg-slate-900 h-full">
+    <div className="bg-slate-900 h-full relative">
       <Navbar />
       <Hero />
       <About />
@@ -19,6 +24,12 @@ const App = () => {
       <Projects />
       <Contact />
       <Footer />
+      <button
+        className="text-slate-900 bg-white hover:bg-[#ddd] rounded-full p-3 fixed hidden md:block right-10 bottom-10"
+        onClick={top}
+      >
+        <FaArrowUp />
+      </button>
     </div>
   );
 };
